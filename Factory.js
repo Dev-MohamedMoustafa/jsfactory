366ab6b529fa568962c9f38b95fa288ca9906be3
Mohamed Moustafa <92525570+Dev-MohamedMoustafa@users.noreply.github.com>
2021-10-28 10:41:06 +0200

    Add files via upload
    initial commit

---
+++ Factory.js
@@ -0,0 +1,45 @@
+/*
+ * this code was written by Mohamed Moustafa
+ * As a preview of his knowledge in Design Patterns
+ * 
+ */
+
+var Factory = function () {
+    this.createEmployee = function (type) {
+        var employee;
+
+        if (type === "fulltime") {
+            employee = new FullTime();
+        } else if (type === "parttime") {
+            employee = new PartTime();
+        } else if (type === "temporary") {
+            employee = new Temporary();
+        } else if (type === "contractor") {
+            employee = new Contractor();
+        }
+
+        employee.type = type;
+
+        employee.say = function () {
+            console.log(this.type + ": rate " + this.hourly + "/hour");
+        }
+
+        return employee;
+    }
+}
+
+var FullTime = function () {
+    this.hourly = "$12";
+};
+
+var PartTime = function () {
+    this.hourly = "$11";
+};
+
+var Temporary = function () {
+    this.hourly = "$10";
+};
+
+var Contractor = function () {
+    this.hourly = "$15";
+};
